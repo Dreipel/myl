6df85ff8677de5ec481fb4a3594dad3086310a02
Jorge Almonacid Almonacid <49615821+Dreipel@users.noreply.github.com>
2025-04-19 21:40:25 -0400

    se agrega nuevo log
    nuevo log

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // 3rd Party Modules 
 const express = require('express');
 const cors = require("cors");
+const winston = require('winston');
 require('dotenv/config'); 
   
 // Local Modules 
@@ -39,6 +40,13 @@ app.use('/Races',raceRoute);
 app.use('/Types',typeRoute);
 
 connect();
+
+process.on('uncaughtException', function (err) {
+    if (err) {
+        console.log("caughtException but no error msg" + err.stack);
+        process.exit(1);
+    }
+});
   
 // Server Listen Along with Database  
 // connection(in case of data persistence) 
@@ -49,3 +57,31 @@ app.listen(PORT, (error) =>{
         console.log("Error occurred, server can't start", error); 
     } 
 );
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or higher to `error.log`
+        //   (i.e., error, fatal, but not other levels)
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        //
+        // - Write all logs with importance level of `info` or higher to `combined.log`
+        //   (i.e., fatal, error, warn, and info, but not trace)
+        //
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
+}
